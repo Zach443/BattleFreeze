b32f64c19ccd56d7f7a60ebdf86d44fbc878caf0
Zach <zbfiorito@gmail.com>
2014-05-10 13:47:14 -0500

    Inital Commit

---
+++ src/me/zach/BattleFreeze/BattleFreeze.java
@@ -0,0 +1,37 @@
+package me.zach.BattleFreeze;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class BattleFreeze extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public void onDisable(){
+		getLogger().info("BYE BYE!");
+	}
+
+	@EventHandler
+	public void onEntityDamage(EntityDamageEvent event) {
+		if(!(event instanceof EntityDamageByEntityEvent)) {
+			EntityDamageByEntityEvent edbeEvent = (EntityDamageByEntityEvent)event;
+			if ((edbeEvent.getDamager() instanceof Snowball)) {
+				LivingEntity entity = (LivingEntity) event.getEntity();
+				entity.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 500, 10), true);
+				
+			}
+		}
+	}
+
+}
